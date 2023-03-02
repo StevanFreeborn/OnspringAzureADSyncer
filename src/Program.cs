7bde4b54b42f459ad7988d5388d2f594a0ca968b
StevanFreeborn <=>
2023-03-01 22:46:24 -0600

    chore: added system.commandline.hosting as dependency

--- src/Program.cs
+++ src/Program.cs
@@ -2,6 +2,10 @@
 {
   internal static void Main(string[] args)
   {
-    Console.WriteLine("Hello World!");
+    Syncer
+    .CreateOptions()
+    .CreateRootCommand()
+    .BuildCommandLine()
+    .UseHost(_ => Host.CreateDefaultBuilder())
   }
 }
