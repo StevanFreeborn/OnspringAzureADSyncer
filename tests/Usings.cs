9f9cce139027fd2a80e5c39e62023370b99b8d6c
StevanFreeborn <=>
2023-03-04 10:47:11 -0600

    chore: add moq and fluent assertions as dependencies to test project

--- tests/Usings.cs
+++ tests/Usings.cs
@@ -1,1 +1,11 @@
+global using FluentAssertions;
+
+global using Moq;
+
+global using OnspringAzureADSyncer.Interfaces;
+global using OnspringAzureADSyncer.Models;
+global using OnspringAzureADSyncer.Services;
+
+global using Serilog;
+
 global using Xunit;
