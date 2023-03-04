1ddd4e36cc00cc157d8705c437208433956c9b16
StevanFreeborn <=>
2023-03-04 11:08:41 -0600

    chore: added script for quickly create local code coverage report

---
+++ tests/scripts/test-coverage.ps1
@@ -0,0 +1,8 @@
+Remove-Item -Path TestResults -Recurse -Force
+
+dotnet test --collect:"XPlat Code Coverage;Include=[OnspringAzureADSyncer]*"
+
+dotnet reportgenerator `
+-reports:TestResults\*\coverage.cobertura.xml `
+-targetdir:TestResults\coveragereport `
+-reporttypes:Html_Dark;
