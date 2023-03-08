000a2b10290966f7f9679446b0d457e6a790043c
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-03-08 15:30:11 -0600

    fix: issue with serilog not logging to files

--- src/Models/Syncer.cs
+++ src/Models/Syncer.cs
@@ -13,11 +13,20 @@ public class Syncer : ISyncer
 
   public async Task<int> Run()
   {
-    _logger.Information("Starting syncer");
+    Console.ForegroundColor = ConsoleColor.DarkBlue;
+
+    var startMsg = "Starting syncer";
+    Console.WriteLine(startMsg);
+    _logger.Information(startMsg);
 
     if (await _processor.VerifyConnections() is false)
     {
-      _logger.Fatal("Unable to connect to Onspring and/or Azure AD");
+      Console.ForegroundColor = ConsoleColor.Red;
+
+      var connErrorMsg = "Unable to connect to Onspring and/or Azure AD";
+      Console.WriteLine(connErrorMsg);
+      _logger.Fatal(connErrorMsg);
+
       return 2;
     }
 
@@ -25,12 +34,20 @@ public class Syncer : ISyncer
     // TODO: Validate that mapped fields exist in Onspring
     // TODO: Validate that mapped properties exist for Azure AD resource
 
+    Console.ForegroundColor = ConsoleColor.DarkBlue;
+    Console.WriteLine("Connected successfully to Onspring and Azure AD");
     _logger.Information("Connected successfully to Onspring and Azure AD");
 
-    _logger.Information("Setting default field mappings");
+    var setDefaultFieldsMsg = "Setting default field mappings";
+    Console.WriteLine(setDefaultFieldsMsg);
+    _logger.Information(setDefaultFieldsMsg);
+
     await _processor.SetDefaultFieldMappings();
 
-    _logger.Information("Syncing groups");
+    var syncGroupsMsg = "Syncing groups";
+    Console.WriteLine(syncGroupsMsg);
+    _logger.Information(syncGroupsMsg);
+
     await _processor.SyncGroups();
 
     // TODO: Sync users
@@ -39,7 +56,9 @@ public class Syncer : ISyncer
 
     // TODO: Reconcile users status
 
-    _logger.Information("Syncer finished");
+    var exitMsg = "Syncer finished";
+    Console.WriteLine(exitMsg);
+    _logger.Information(exitMsg);
 
     await Log.CloseAndFlushAsync();
 
